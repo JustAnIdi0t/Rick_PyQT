aef140f3ea9875ffc35f54e35b1ad1cd56676fd8
JustAnIdi0t <87780666+JustAnIdi0t@users.noreply.github.com>
2021-09-12 21:06:55 -0600

    Update RickAndSon.py

--- RickAndSon.py
+++ RickAndSon.py
@@ -1,1 +1,15 @@
-print("Hello world")
+#This program takes in information from a user
+import sys
+
+def user_data():
+    name = input("Please enter a name:\n")
+    age = input("Please enter an age:\n")
+    gender = input("Please enter a gender:\n")
+    hobby = input("Please enter a hobby:\n")
+
+answer = input("Enter a 1 to create an account, enter anything else to exit:\n")
+if answer == '1':
+    user_data()
+else:
+    sys.exit()
+
